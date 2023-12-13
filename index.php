854ac906eefc92eee7dba5eab0ffbf26b67b3702
Andrey <drey.owen@gmail.com>
2023-12-13 16:01:25 -0500

    Fixed polls.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                     <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="index.php">Home</a></li>
                     <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="posts.php">Posts</a></li>
                     <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="community.php">Community</a></li>
-                    <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="community.php">Polls</a></li>
+                    <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="polls.php">Polls</a></li>
                 </ul>
                 <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">EcoTrack</a>
             </div>
---
+++ polls.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>EcoTrack</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <h1 class="site-heading text-center text-faded d-none d-lg-block">
+            <span class="site-heading-upper text-primary mb-3">Protecting the environment together</span>
+            <span class="site-heading-lower">EcoTrack</span>
+        </h1>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+        <div class="container">
+            <div class="d-flex align-items-center">
+                <ul class="navbar-nav">
+                    <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="index.php">Home</a></li>
+                    <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="posts.php">Posts</a></li>
+                    <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="community.php">Community</a></li>
+                    <li class="nav-item mb-2"><a class="nav-link text-uppercase" href="polls.php">Polls</a></li>
+                </ul>
+                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">EcoTrack</a>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <li class="nav-item">
+                    <button class="btn btn-primary">Create a User</button>
+                </li>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Polls Section -->
+    <section class="container mt-4">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <img src="assets/img/icons/poll_1.jpg" alt="Poll 1 Image" class="rounded-circle me-3" style="width: 70px; height: 70px;">
+                        <h5 class="card-title"><strong>Poll 1 Title</strong></h5>
+                        <p class="card-text">Poll 1 description or question.</p>
+                        <a href="#" class="btn btn-primary">Vote</a>
+                        <span class="post-date position-absolute top-0 end-0">Posted on March 15, 2023</span>
+                    </div>
+                </div>
+
+                <!-- Repeat structure for additional polls -->
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Community Guidelines</h5>
+                        <ul>
+                            <li><span>&#127793;</span> Be respectful to others and their opinions.</li>
+                            <li><span>&#127793;</span> Avoid posting offensive or inappropriate content.</li>
+                            <li><span>&#127793;</span> Share helpful and informative content about eco-friendly practices.</li>
+                            <!-- Add more guidelines as needed -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer text-faded text-center py-5">
+        <div class="container"><p class="m-0 small">Copyright &copy; EcoTrack 2023</p></div>
+    </footer>
+    <!-- Bootstrap core JS and other scripts -->
+    <!-- Include necessary Bootstrap and other JS libraries -->
+    <!-- Your custom scripts -->
+    <script>
+        // Add your custom JavaScript here
+    </script>
+</body>
+</html>
