ae1aea67006773c938707ca821fcc6c504894ffd
Andrey <drey.owen@gmail.com>
2023-12-13 18:22:25 -0500

    Reworked Post Button

--- posts.php
+++ posts.php
@@ -32,28 +32,29 @@ function generateCards(){
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>EcoTrack</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    <body>
-        <header>
-            <h1 class="site-heading text-center text-faded d-none d-lg-block">
-                <span class="site-heading-upper text-primary mb-3">Protecting the environment together</span>
-                <span class="site-heading-lower">EcoTrack</span>
-            </h1>
-        </header>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>EcoTrack</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <h1 class="site-heading text-center text-faded d-none d-lg-block">
+            <span class="site-heading-upper text-primary mb-3">Protecting the environment together</span>
+            <span class="site-heading-lower">EcoTrack</span>
+        </h1>
+    </header>
+
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
         <div class="container">
             <div class="d-flex align-items-center">
                 <ul class="navbar-nav">
@@ -72,32 +73,36 @@ function generateCards(){
             <!-- Create User Button -->
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <li class="nav-item">
-                <a class="btn btn-primary" href="create.php"><strong>Create a User</strong></a>
+                    <a class="btn btn-primary" href="create.php"><strong>Create a User</strong></a>
                 </li>
             </div>
         </div>
     </nav>
+
     <br>
+
     <!-- Create Post Button -->
     <div class="text-center">
         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createPostModal"><Strong>Create Post</Strong></button>
-            </div>
+    </div>
 
-        <!-- Create Post Modal -->
-            <div class="modal fade" id="createPostModal" tabindex="-1" aria-labelledby="createPostModalLabel" aria-hidden="true">
-            <!-- Modal content as provided earlier -->
-            </div>
+    <!-- Create Post Modal -->
+    <div class="modal fade" id="createPostModal" tabindex="-1" aria-labelledby="createPostModalLabel" aria-hidden="true">
+        <!-- Modal content as provided earlier -->
+    </div>
 
-        <!-- Post Cards -->
-        <section class="page-section clearfix">
-            <div class="container-fluid">
-                <?php generateCards(); ?>
-            </div>
-        </section>
+    <!-- Post Cards -->
+    <section class="page-section clearfix">
+        <div class="container-fluid">
+            <?php generateCards(); ?>
+        </div>
+    </section>
 
-        <!-- Footer -->
-        <footer class="footer text-faded text-center py-5">
-            <div class="container"><p class="m-0 small">Copyright &copy; EcoTrack 2023</p></div>
-        </footer>
-    </body>
+    <!-- Footer -->
+    <footer class="footer text-faded text-center py-5">
+        <div class="container">
+            <p class="m-0 small">Copyright &copy; EcoTrack 2023</p>
+        </div>
+    </footer>
+</body>
 </html>
